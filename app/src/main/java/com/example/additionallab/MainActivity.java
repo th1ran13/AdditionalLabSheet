acf959111812124dbdf553a87228f5274d458e9f
th1ran13 <thiraniherath@gmail.com>
2021-08-20 21:48:30 +0530

    initial commit

---
+++ app/src/main/java/com/example/additionallab/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.additionallab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button start , stop;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.bt_start);
+        stop = (Button) findViewById(R.id.bt_stop);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+    }
+}
